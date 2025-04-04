9fb253220f491053d33833cb62ae7b877305b20a
SYShopnil <sadmanishopnil@gmail.com>
2025-04-04 18:01:50 +0600

    change on after install sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
 cd /home/ec2-user/nest-app
+
+# Make sure the deploy user owns the files
+sudo chown -R ec2-user:ec2-user .
+
+# Install dependencies
 npm install
+
+# Pull .env from SSM
 aws ssm get-parameter \
   --name "/prod/nestjs/env" \
   --with-decryption \
   --query "Parameter.Value" \
   --output text > .env
+
+# Secure the .env file
+chmod 600 .env
